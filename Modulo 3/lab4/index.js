606dec4df8919144d33519badeddaffa4da4d740
jonathan <makumedia@gmail.com>
2017-08-12 13:04:41 -0500

    clase 3

---
+++ Modulo 3/lab4/index.js
@@ -0,0 +1,6 @@
+var a;
+var b = 'variable';
+var b = '5';
+var c = true;
+var d = ['a', 'b', 'c'];
+var a;
---
+++ Modulo 3/lab4/index.ts
@@ -0,0 +1,6 @@
+let a:string;
+let b:string='variable';
+let b:number='5';
+let c:boolean=true;
+let d:array <string>=['a','b','c'];
+let a: string;
