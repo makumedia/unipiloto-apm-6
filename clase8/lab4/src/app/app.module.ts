a72862d1b46ec77a2821a53a7dfaa1bfa75737b5
jonathan <makumedia@gmail.com>
2017-09-02 13:01:27 -0500

    clase 7 y 8

---
+++ clase8/lab4/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {InMemoryWebApiModule} from "angular2-in-memory-web-api";
+import {InMemoryProductService} from "./mocks/in-memory-product.service";
+
+import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import {ProductsService} from "./services/products.service";
+import { PagesComponent } from './pages/pages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductDetailComponent,
+    PagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryProductService)
+  ],
+  providers: [InMemoryProductService, ProductsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
